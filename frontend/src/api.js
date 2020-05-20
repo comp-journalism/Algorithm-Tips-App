a22839f3c3eb6908b2928a7ac2033e86a30e7187
emallson <emallson@atlanis.net>
2020-05-20 16:36:56 -0400

    uwsgi tweaks + frontend now uses /api endpoint on prod

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,8 @@
-const BASE_URL = "http://localhost:8080/api/";
+let HOST = "";
+if (process.env.NODE_ENV !== 'production') {
+    HOST = "http://localhost:8080";
+}
+const BASE_URL = `${HOST}/api/`;
 export function api_url(path) {
     return `${BASE_URL}${path}`;
 }
