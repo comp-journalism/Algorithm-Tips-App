28973122a75d3c09c261ed145f0eb45768267863
emallson <emallson@atlanis.net>
2020-06-04 16:02:42 -0400

    alert render test now freezes time

--- api/mail_test.py
+++ api/mail_test.py
@@ -30,6 +30,7 @@ def test_confirmation_no_reconfirm(sqlite_connection, confirmed_email, alert_app
         mock_mail_singleton.assert_not_called()
 
 
+@freeze_time('2020-06-04')
 def test_alert_render(alert_app, snapshot):
     ALERT = {'send_id': 1, 'user_id': 1, 'federal_source': 'Federal Agency - Executive', 'regional_source': 'exclude', 'local_source': None, 'frequency': 0, 'recipient': 'test@test.net', 'filter': '', 'alert_id': 1, 'send_date': datetime(2020, 6, 3, 14, 46, 27, 817514)}
     LEADS = [{'name': "FEMA's Climate Impact Model", 'link': 'http://db.algorithmtips.org/lead/6933'}]
