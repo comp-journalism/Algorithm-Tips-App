4f5371113c8df0753dcf582f698079f5e188b684
David Smith <emallson@emallson.net>
2020-06-29 13:19:55 -0400

    no longer use internal fields to get google email
    the fields rotate, need to use getters
    
    autoformat on save continues to troll me

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,57 +1,57 @@
-import { api_url } from '../api';
-import axios from 'axios';
-import EventBus from '../event-bus';
+import { api_url } from "../api";
+import axios from "axios";
+import EventBus from "../event-bus";
 
-export const STORE_USER = 'STORE_USER';
-export const CLEAR_USER = 'CLEAR_USER';
-export const SET_LOGIN_STATUS = 'SET_LOGIN_STATUS';
+export const STORE_USER = "STORE_USER";
+export const CLEAR_USER = "CLEAR_USER";
+export const SET_LOGIN_STATUS = "SET_LOGIN_STATUS";
 
 export default {
-    namespaced: true,
-    state: {
-        user: null,
-        loggedIn: false,
+  namespaced: true,
+  state: {
+    user: null,
+    loggedIn: false
+  },
+  mutations: {
+    [STORE_USER](state, user) {
+      state.user = user;
     },
-    mutations: {
-        [STORE_USER](state, user) {
-            state.user = user;
-        },
-        [CLEAR_USER](state) {
-            state.user = null;
-        },
-        [SET_LOGIN_STATUS](state, status) {
-            state.loggedIn = status;
-        }
+    [CLEAR_USER](state) {
+      state.user = null;
     },
-    actions: {
-        async login({ commit }, user) {
-            commit(STORE_USER, user);
-            await axios({
-                method: "POST",
-                url: api_url("auth/signin"),
-                data: { id_token: user.wc.id_token },
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                withCredentials: true
-            });
-
-            commit(SET_LOGIN_STATUS, true);
-            EventBus.$emit('login');
-        },
-        async logout({ commit }) {
-            await axios({
-                method: 'GET',
-                url: api_url('auth/signout'),
-                withCredentials: true
-            });
-
-            commit(SET_LOGIN_STATUS, false);
-            EventBus.$emit('logout');
-        }
-    },
-    getters: {
-        signedIn: (state) => state.loggedIn,
-        email: (state) => state.user ? state.user.Tt.Du : '',
+    [SET_LOGIN_STATUS](state, status) {
+      state.loggedIn = status;
     }
-}
+  },
+  actions: {
+    async login({ commit }, user) {
+      commit(STORE_USER, user);
+      await axios({
+        method: "POST",
+        url: api_url("auth/signin"),
+        data: { id_token: user.getAuthResponse().id_token },
+        headers: {
+          "Content-Type": "application/json"
+        },
+        withCredentials: true
+      });
+
+      commit(SET_LOGIN_STATUS, true);
+      EventBus.$emit("login");
+    },
+    async logout({ commit }) {
+      await axios({
+        method: "GET",
+        url: api_url("auth/signout"),
+        withCredentials: true
+      });
+
+      commit(SET_LOGIN_STATUS, false);
+      EventBus.$emit("logout");
+    }
+  },
+  getters: {
+    signedIn: state => state.loggedIn,
+    email: state => (state.user ? state.user.getBasicProfile().getEmail() : "")
+  }
+};
